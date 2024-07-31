b350799a74075b599a1f15333ad83555b7202362
mikejlong60 <mikejlong60@gmail.com>
2024-07-31 11:54:19 -0400

    Created test functiokns

--- chapter5/mystring_test.c
+++ chapter5/mystring_test.c
@@ -71,48 +71,73 @@ void test_strncat3_concat_1() {
     assert(strcmp3(result, expected) == 0);
 }
 
-int main() {
-    test_strcat3();
-    test_strncat3_concat_all_giant_n();
-    test_strncat3_concat_all();
-    test_strncat3_concat_1();
-    test_strncat3_concat_2();
-    //test strlen2
-     char expected[] = {'h','e','l','l','o', '\0'};
+void test_strlen2() {
+    char expected[] = {'h','e','l','l','o', '\0'};
     int a = strlen2(expected);
     assert(strlen2(expected) == 5);
+}
 
+void test_strend_true1() {
     //test returns 1 if string s2 occurs at the end of string t2. 0 otherwise
     char t2[] = {'l','l','o','\0'};
     char s3[] = {'o', '\0'};
     assert(strend2(t2, s3) == 1);
-    //This should return false
-    char t3[] = {'l','l','\0'};
-    assert(strend2(t3, s3) == 0);
-    //This should return true
+}
+
+void test_strend_true2() {
+   //This should return true
     char t4[] = {'h','e','l','l','o','\0'};
     char s4[] = {'l','l','o','\0'};
     assert(strend2(t4, s4) == 1);
- 
-    //This should return false
+}
+
+void test_strend_false1() {
+    char s3[] = {'o', '\0'};
+    char t3[] = {'l','l','\0'};
+    assert(strend2(t3, s3) == 0);
+}
+
+void test_strend_false2() {
+   //This should return false
     char t5[] = {'h','o','\0'};
     char s5[] = {'l','l','o','\0'};
     assert(strend2(t5, s5) == 0);
-    
+}
+
+void test_strend_true3() {
    //This should return true
     char t6[] = {'o','\0'};
     char s6[] = {'o','\0'};
     assert(strend2(t6, s6) == 1);
- 
-   //test strcpy2 -- This should return true
+}
+
+
+void test_strcpy2_true() {
+    //test strcpy2 -- This should return true
     char buffer[5];
     char *result = strcpy2(buffer, "hello");
     printf("buffer:%s \n", buffer);
     printf("result:%s \n", result);
     assert(strcmp3(buffer, "hello") == 0);
     assert(strcmp3(result, "hello") == 0);
+}
 
+int main() {
+    test_strcat3();
+    test_strncat3_concat_all_giant_n();
+    test_strncat3_concat_all();
+    test_strncat3_concat_1();
+    test_strncat3_concat_2();
 
-    //test_strncpy2_n_gt_from_size();
-    //test_strncpy2_n_lt_from_size();
+    test_strncpy2_n_gt_from_size();
+    test_strncpy2_n_lt_from_size();
+
+    test_strlen2();
+    test_strend_true1();
+    test_strend_false1();
+    test_strend_true2();
+    test_strend_true3();
+    test_strend_false2();
+
+     test_strcpy2_true();
 }
