23292c1bd5961561a7d49d7fcbc50846464e90a0
mikejlong60 <mikejlong60@gmail.com>
2024-08-15 09:17:40 -0400

    dd

--- chapter8/malloc.c
+++ chapter8/malloc.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
 
 typedef long Align; //for alignment to long boundary
 
@@ -7,19 +9,55 @@ union header { //block header
         union header *ptr; //next block if on free list
         unsigned size; //size of this block
     } s;
-    Align x;
+    Align x; //force alignment of blocks
 };
 typedef union header Header;
 
 static Header base; //empty list to get started
 static Header *freep = NULL; //start of free list
 
+// free -- put block ap in free list
+void free(void *ap) {
+    Header *bp, *p;
+
+    bp = (Header *)ap -1; //point to block header
+    for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+        if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+            break; //freed block at start or end of arena
+
+    if (bp + bp->s.size == p->s.ptr) { // join to upper nbr
+        bp->s.size += p->s.ptr->s.size;
+        bp->s.ptr = p->s.ptr->s.ptr;
+    } else
+        bp->s.ptr = p->s.ptr;
+    
+    if (p + p->s.size == bp) { //join to lower nbr
+        p->s.size += bp->s.size;
+        p->s.ptr = bp->s.ptr;
+    } else
+        p->s.ptr = bp;
+
+    freep = p;
+}
 
 #define NALLOC 1024 // minimum number of units to request
 //morecore -- ask system for more memory
 static Header *morecore(unsigned nu) {
+    char *cp;//, *sbrk(int);
+    Header *up;
+
+    if (nu < NALLOC)
+        nu = NALLOC;
+    cp = sbrk(nu * sizeof(Header));
+    if (cp == (char *) -1) //no space at all
+        return NULL;
+    up = (Header *) cp;
+    up->s.size = nu;
+    free((void *)(up + 1));
+
     return freep;
 }
+
 
 //malloc -- general purpose sorage allocator
 void *mymalloc(unsigned nbytes) {
@@ -53,5 +91,8 @@ void *mymalloc(unsigned nbytes) {
 
 int main() {
 
+    char *p = mymalloc(20000000);
+    for (; ;) 
+        printf("%p\n", p);
 }
 
